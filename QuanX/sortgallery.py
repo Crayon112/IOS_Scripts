ce93f9a742e91747d631904e812b7cf3f6666073
Crayon <karontjl112@gmail.com>
2021-04-01 22:17:55 +0800

    Update sortgallery.py

--- QuanX/sortgallery.py
+++ QuanX/sortgallery.py
@@ -1,32 +1,30 @@
-def read_gallery(path):
-  with open(path, 'r') as f:
-    f = f.read()
-    f = f.split('\n')
-    # 去杂
-    f = [_f.strip() if _f.find("config") != -1 else None for _f in f]
-    # 去重
-    f = list(set(f))
-    # 去空
-    f = f.remove(None)
-    # 排序
-    f.sort()
-    return f
-
 def write(gallery, path):
-  gallery = ',\n'.join([f'\t\t\t{{{g}}}' for g in gallery])
-  pattern = f'''
-{
+    gallery = '\n\t},\n\t{\n\t\t'.join([f'{g}' for g in gallery])
+    pattern = f'''
+{{
     "name": "Crayon112",
     "task": [
+    {{
         {gallery}
+    }}
     ],
     "description": "Scripts Gallery"
-}
+}}
   '''
-  with open(path, 'w') as f:
-    f.write(pattern)
+  
+    with open(path, 'w') as f:
+        f.write(pattern)
+
+def read_gallery(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        lines = [line if line.find('config') != -1 else "" for line in lines]
+        lines = list(set(lines))
+        lines.remove("")
+        return lines
 
 if __name__ == "__main__":
-  path = '.\gallery.json'
-  gallery = read_gallery(path)
-  write(gallery, path)
+    path = 'D:\\gallery.json'
+    gallery = read_gallery(path)
+    write(gallery, path)
