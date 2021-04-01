8f1403e6d2921a04d0c097fa2da6865ee1612aff
Crayon <karontjl112@gmail.com>
2021-04-01 13:59:51 +0800

    Update IP_API.js

--- QuanX/General/IP_API.js
+++ QuanX/General/IP_API.js
@@ -44,7 +44,7 @@ var date = new Date();
 var time = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
 var emoji =  emojis[getRandomInt(emojis.length)]
 
-var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']) + emoji + ' ' + time;
+var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city']) + emoji + '\r\n' + time;
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName']) + '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
